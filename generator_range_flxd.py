789d2725b7a43a8872a67c0c12fe36beb6817e6b
svetimir <ifi@yandex.ru>
2018-09-08 11:57:22 +0300

    0.0.2, misc changes

--- generator_range_flxd.py
+++ generator_range_flxd.py
@@ -1,26 +1,52 @@
-#функция-генератор для создания
-#списков с вещественными числами
-#код генератора взят с stackoverflow
+# -*- coding: utf-8 -*-
 
-#Georgy Yashin, ifi@yandex.ru
-#04.09.2017
+# функция-генератор для создания списков
+# с вещественными числами
+# код генератора взят с stackoverflow
+# табулируемая функция задается интегралом
+
+# AUTHOR: Georgy Yashin, ifi@yandex.ru
+# DATE: 04.09.2017
+# LATEST REVISION: 08.09.2018
 
 import math
 
+# для рисования графика
+import matplotlib.pyplot as plt
+
 def drange(start,stop,step):
+    """функция-генератор списка"""
     r=start
     while r<stop:
         yield r
         r+=step
 
-def f(x): return math.exp(-pow(x,2))
+def f(x):
+    """0.1"""
+    return math.sin(x)
 
-xmin=-10.0
-xmax=10.0
-n=200
-dx=(xmax-xmin)/n
+def tr_integral(f,xmin,xmax,n):
+    """процедура интегрирования по методу трапеций"""
+    dx=(xmax-xmin)/n
+    area=0
+    x=xmin
+    for i in range(n):
+        area+=dx*(f(x)+f(x+dx))/2
+        x+=dx
+    return area
 
-#табулируем функцию в пределах xmin,xmax
-xi=drange(xmin,xmax,dx)
-a=[f(x) for x in xi]
-print(a)
+def g(x):
+    return tr_integral(f,-1e6,x,1000)
+
+if __name__=="__main__":
+    xmin=-5.0
+    xmax=5.0
+    n=400
+    dx=(xmax-xmin)/n
+# табулируем функцию в пределах xmin,xmax
+    print("testing "+drange.__name__)
+    xi=drange(xmin,xmax,dx)
+    a=[g(x) for x in xi]
+    print(a)
+    plt.plot(a)
+    plt.show()
--- generator_range_w_integral_flxd.py
+++ generator_range_w_integral_flxd.py
@@ -1,9 +1,12 @@
-#функция-генератор для создания
-#списков с вещественными числами
-#код генератора взят с stackoverflow
+# -*- coding: utf-8 -*-
 
-#Georgy Yashin, ifi@yandex.ru
-#04.09.2017
+# функция-генератор для создания
+# списков с вещественными числами
+# код генератора взят с stackoverflow
+
+# AUTHOR: Georgy Yashin, ifi@yandex.ru
+# DATE: 04.09.2017
+# LATEST REVISION: 08.09.2018
 
 import math
 import ch_integr_flxd as ci
@@ -16,15 +19,15 @@ def drange(start,stop,step):
 
 def f(x): return math.exp(-pow(x,2)+math.cos(x))
 
-xmin=-10.0
-xmax=10.0
-n=200
-dx=(xmax-xmin)/n
 
-#табулируем функцию в пределах xmin,xmax
-xi=drange(xmin,xmax,dx)
-a=[f(x) for x in xi]
-print(a)
-
-#возьмем интеграл от f(x) на xmin,xmax методом трапеций
-print(ci.tr_integral(f,xmin,xmax,n))
+if __name__=="__main__":
+    xmin=-10.0
+    xmax=10.0
+    n=200
+    dx=(xmax-xmin)/n
+    # табулируем функцию в пределах xmin,xmax
+    xi=drange(xmin,xmax,dx)
+    a=[f(x) for x in xi]
+    print(a)
+    # возьмем интеграл от f(x) на xmin,xmax методом трапеций
+    print(ci.tr_integral(f,xmin,xmax,n))
